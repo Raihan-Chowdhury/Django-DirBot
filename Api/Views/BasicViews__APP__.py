75389fb4152db238d1a8cde95cf222b355cea903
Raihan Chowdhury <62661424+Raihan-Chowdhury@users.noreply.github.com>
2020-11-13 19:17:08 +0600

    Create BasicViews__APP__.py

---
+++ Api/Views/BasicViews__APP__.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from rest_framework import generics,permissions
+from .serializers import Postserializer
+
+from .models import Post
+
+
+# Create your views here.
+class ShowList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
+        
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = Postserializer
+
+    def perform_create(self, serializer):
+        serializer.save(poster = self.request.user)
