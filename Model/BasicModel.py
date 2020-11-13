601ac240eb6526a08fc0b79e93bc56d0bfc42af5
Raihan Chowdhury <62661424+Raihan-Chowdhury@users.noreply.github.com>
2020-11-13 19:06:00 +0600

    Update BasicModel.py

--- Model/BasicModel.py
+++ Model/BasicModel.py
@@ -1,15 +1,17 @@
 class Post(models.Model):
+    #Fields
     title = models.CharField(max_length=100)
     url = models.URLField()
     poster = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    
+    # Return Something (Helps to detect objects in admin)
     def __str__(self):
         return str(self.title)
-        
+    # To Avoid Error
     objects = models.Manager()
-
+    # To Avoid Errors + ... 
     class Meta:
+        # Ordering Datas
         ordering = ['-created']
         managed = True
         db_table = 'Post'
